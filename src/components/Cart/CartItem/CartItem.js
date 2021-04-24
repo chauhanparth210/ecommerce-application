c94f051ac68966c6df8a946ee4392da0841debf2
chauhanparth210 <00chauhanparth@gmail.com>
2021-04-24 14:32:29 +0530

    chore: change the cart item design

--- src/components/Cart/CartItem/CartItem.js
+++ src/components/Cart/CartItem/CartItem.js
@@ -10,45 +10,57 @@ const CartItem = ({ item, onUpdateCartQty, onRemoveFromCart }) => {
   const handleRemoveFromCart = (lineItemId) => onRemoveFromCart(lineItemId);
 
   return (
-    <Column size="4" sizeM="6" sizeS="12" sizeXS="12">
-      <Card shadow="medium" className="m-7 overflow-hidden">
-        {media.source && (
-          <div
-            className="d-flex align-items-center overflow-hidden bg-secondary-lightest"
-            style={{ height: '280px' }}
-          >
-            <img src={media.source} alt={name} className="w-100" />
-          </div>
-        )}
-        <div className="d-flex flex-column px-5 py-4">
-          <div className="d-flex justify-content-between mt-4">
-            <Heading size="l">{name}</Heading>
-            <Heading size="m" appearance="subtle">
-              {price.formatted_with_symbol}
-            </Heading>
-          </div>
-          <div className="d-flex justify-content-between align-items-center mt-6">
-            <div className="d-flex align-items-center">
-              <Button
-                appearance="basic"
-                icon="add"
-                size="regular"
-                onClick={() => handleUpdateCartQty(id, quantity + 1)}
-              />
-              <Text className="mx-5">{quantity}</Text>
-              <Button
-                appearance="basic"
-                icon="remove"
-                size="regular"
-                onClick={() => handleUpdateCartQty(id, quantity - 1)}
-              />
+    <Column size="12">
+      <Card shadow="medium" className="mx-7 my-5 d-flex">
+        <Column size="3" sizeXS="4" sizeS="4" sizeM="4" sizeL="3" sizeXL="3">
+          {media.source && (
+            <div className="d-flex align-items-center overflow-hidden bg-secondary-lightest cart-image">
+              <img src={media.source} alt={name} className="w-100" />
             </div>
+          )}
+        </Column>
+        <div className="ml-4 p-4 d-flex flex-grow-1 flex-wrap justify-content-between">
+          <Heading size="l">{name}</Heading>
+          <div className="d-flex flex-column justify-content-between">
             <Button
               appearance="alert"
+              icon="delete"
+              className="align-self-end"
               onClick={() => handleRemoveFromCart(item.id)}
+            />
+            <div
+              className="d-flex align-items-end justify-content-between"
+              style={{ width: '230px' }}
             >
-              Remove
-            </Button>
+              <div className="show-quantity">
+                <Text
+                  size="large"
+                  weight="medium"
+                  className="mb-4"
+                  appearance="subtle"
+                >
+                  Quantity
+                </Text>
+                <div className="d-flex align-items-center">
+                  <Button
+                    appearance="basic"
+                    icon="add"
+                    size="regular"
+                    onClick={() => handleUpdateCartQty(id, quantity + 1)}
+                  />
+                  <Text className="mx-5">{quantity}</Text>
+                  <Button
+                    appearance="basic"
+                    icon="remove"
+                    size="regular"
+                    onClick={() => handleUpdateCartQty(id, quantity - 1)}
+                  />
+                </div>
+              </div>
+              <Heading size="m" appearance="subtle">
+                {price.formatted_with_symbol}
+              </Heading>
+            </div>
           </div>
         </div>
       </Card>
